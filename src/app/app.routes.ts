5bec969dcd73e837e293830bf9b1c45491b4c512
Hang Liu <hang.loiu@focusnet.com.au>
2025-06-05 10:08:03 +0800

    Add root path route，Fix routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { RequirementsWizard } from './requirements-wizard/requirements-wizard';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+         path: '',
+         component: HomeComponent,
+         title: 'Main Page'
+    },    
+
+
+    {
+        path: 'requirements/wizard',
+        loadComponent: () => import('./requirements-wizard/requirements-wizard').then(m => m.RequirementsWizard),
+        title:'Requirements Wizard'
+    }
+];
