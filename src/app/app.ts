c832708e638d7e90ae53a22caf20435d60ffc492
Hang Liu <hang.loiu@focusnet.com.au>
2025-06-05 10:10:16 +0800

    Add RouterModule to the main component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
