270c128ab0627fb839c25878cbe7b1ce34e1004d
Hang Liu <hang.loiu@focusnet.com.au>
2025-06-09 10:59:45 +0800

    Adding step logic

--- src/app/requirements-wizard/requirements-wizard.ts
+++ src/app/requirements-wizard/requirements-wizard.ts
@@ -7,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrl: './requirements-wizard.css'
 })
 export class RequirementsWizard {
+  step = 1;
+
+  client = {
+    name: '',
+    tenantID: '',
+    services: [] as string[]
+  };
+
+   availableServices = [
+    { name: 'Active Directory Certificate Services', selected: false },
+    { name: 'DNS Server', selected: false },
+    { name: 'DHCP Server', selected: false },
+    { name: 'File Services', selected: false }
+  ];
+
+  nextStep() {
+    if (this.step === 2) {
+      this.client.services = this.availableServices
+        .filter(s => s.selected)
+        .map(s => s.name);
+    }
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+
+  submit() {
+    console.log('Final client config:', this.client);
+  }
+
 
 }
