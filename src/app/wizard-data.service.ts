15d9fb3a867fc48be46e644f7341a8e34f31bda4
Hang Liu <hang.loiu@focusnet.com.au>
2025-06-10 15:02:04 +0800

    support saving multiple wizard form entries

--- src/app/wizard-data.service.ts
+++ src/app/wizard-data.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class WizardDataService {
   private data: any = {};
+  private savedEntries: any[] = [];
 
   setData(step: string, value: any) {
     this.data[step] = value;   //Set or save data for a certain step
@@ -20,11 +21,20 @@ export class WizardDataService {
 
   saveWizardData(formData: any) {
     this.data = formData;
+    this.savedEntries.push(formData);
+  }
+
+  saveWizardEntry(entry: any) {
+    this.savedEntries.push(entry); 
   }
 
   getWizardData() {
     return this.data;
   }
+
+  getAllEntries() {
+  return this.savedEntries;
+}
 
   resetData() {
     this.data = {};
