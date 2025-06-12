c81f69fc7685edd462e394aaf8300b7f20aebf67
Hang Liu <hang.loiu@focusnet.com.au>
2025-06-12 10:33:52 +0800

    add support module details component route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,5 +20,22 @@ export const routes: Routes = [
         path: 'requirements/wizard/module-user-management',
         loadComponent: () => import('./requirements-wizard/module-user-management.component').then(m => m.ModuleUserManagementComponent),
         title: 'User Management Details'
-  },
+    },
+
+    {
+        path: 'requirements/wizard/module-billing',
+        loadComponent: () => import('./requirements-wizard/module-user-billing.component').then(m => m.ModuleUserBillingComponent),
+        title: 'Billing Details'
+    },
+    {
+        path: 'requirements/wizard/module-reporting',
+        loadComponent: () => import('./requirements-wizard/module-user-reporting.component').then(m => m.ModuleUserReportingComponent),
+        title: 'Reporting Details'
+    },
+
+    {
+        path: 'requirements/wizard/module-support',
+        loadComponent: () => import('./requirements-wizard/module-user-support.component').then(m => m.ModuleUserSupportComponent),
+        title: 'Support Details'
+    },
 ];
