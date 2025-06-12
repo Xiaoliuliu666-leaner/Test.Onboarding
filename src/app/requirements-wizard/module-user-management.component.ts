c3f92ec4f02038ee6a08a77351c34bae24958dce
Hang Liu <hang.loiu@focusnet.com.au>
2025-06-12 16:10:47 +0800

    Delete Comments

--- src/app/requirements-wizard/module-user-management.component.ts
+++ src/app/requirements-wizard/module-user-management.component.ts
@@ -12,7 +12,7 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./module-user-management.component.scss']
 })
 export class ModuleUserManagementComponent {
-  // Used to collect user input data
+
   detail = {
     adminCount: 1,
     notes: ''
@@ -29,10 +29,10 @@ export class ModuleUserManagementComponent {
   const next = this.wizardDataService.getNextModule('user-management');
   console.log('next:', next);
 
-  // Save data to global service
+
   this.wizardDataService.setModuleDetail('user-management', this.detail);
 
-  // Jump to the next checked module or notes
+
   if (next) {
     this.router.navigate(['/requirements/wizard/module-' + next]);
   } else {
