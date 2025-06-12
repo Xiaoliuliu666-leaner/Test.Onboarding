0f5a3935090f68f7190aa68853b4415064140c5b
Hang Liu <hang.loiu@focusnet.com.au>
2025-06-12 10:33:13 +0800

    add support module details component

---
+++ src/app/requirements-wizard/module-user-support.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { WizardDataService } from '../wizard-data.service';
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  standalone: true,
+  selector: 'app-module-user-support',
+  templateUrl: './module-user-support.component.html',
+  imports: [FormsModule],
+  styleUrls: ['./module-user-support.component.scss']
+})
+export class ModuleUserSupportComponent {
+  // Used to collect user input data
+  detail = {
+    supportLevel: 1,
+    supportNotes: ''
+  };
+
+  constructor(
+    private router: Router,
+    private wizardDataService: WizardDataService
+  ) {}
+
+  onNext() {
+    // Save data to global service
+    this.wizardDataService.setModuleDetail('support', this.detail);
+    // Jump to the next checked module or notes
+    const next = this.wizardDataService.getNextModule('support');
+    if (next) {
+      this.router.navigate(['/requirements-wizard/module-' + next]);
+    } else {
+      this.router.navigate(['/requirements-wizard/notes']);
+    }
+  }
+}
