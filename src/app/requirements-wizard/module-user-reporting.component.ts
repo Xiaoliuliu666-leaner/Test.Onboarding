e3e506df7dceb761c3b59a2b706e65a2b1a89402
Hang Liu <hang.loiu@focusnet.com.au>
2025-06-12 10:28:01 +0800

    add reporting module details component

---
+++ src/app/requirements-wizard/module-user-reporting.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { WizardDataService } from '../wizard-data.service';
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  standalone: true,
+  selector: 'app-module-user-reporting',
+  templateUrl: './module-user-reporting.component.html',
+  imports: [FormsModule],
+  styleUrls: ['./module-user-reporting.component.scss']
+})
+export class ModuleUserReportingComponent {
+  // Used to collect user input data
+  detail = {
+    reportTypes: 1,
+    reportingNotes: ''
+  };
+
+  constructor(
+    private router: Router,
+    private wizardDataService: WizardDataService
+  ) {}
+
+  onNext() {
+    // Save data to global service
+    this.wizardDataService.setModuleDetail('reporting', this.detail);
+    // Jump to the next checked module or notes
+    const next = this.wizardDataService.getNextModule('reporting');
+    if (next) {
+      this.router.navigate(['/requirements-wizard/module-' + next]);
+    } else {
+      this.router.navigate(['/requirements-wizard/notes']);
+    }
+  }
+}
