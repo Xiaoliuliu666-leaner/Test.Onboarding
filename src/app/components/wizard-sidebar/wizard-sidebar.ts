8099c9d6218e9491ce6d07b8be72b74379c4de3e
Hang Liu <hang.loiu@focusnet.com.au>
2025-06-17 14:42:22 +0800

    add integrationSteps and integrationStepIndex input for sidebar

--- src/app/components/wizard-sidebar/wizard-sidebar.ts
+++ src/app/components/wizard-sidebar/wizard-sidebar.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Input } from '@angular/core';
+import { Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-wizard-sidebar',
@@ -12,10 +14,27 @@ import { CommonModule } from '@angular/common';
 
 export class WizardSidebarComponent {
   @Input() activeMenu: string = '';
+  @Input() integrationSteps: string[] = [];
+  @Input() integrationStepIndex: number|null = null;
+  @Output() integrationStepChange = new EventEmitter<number>();
+
+
   menuList = [
     { key: 'User Management', label: 'User Management' },
     { key: 'Billing', label: 'Billing' },
     { key: 'Reporting', label: 'Reporting' },
-    { key: 'Support', label: 'Support' }
+    { key: 'Support', label: 'Support' },
+    { key: 'Workflows', label: 'Workflows'},
+    { key: 'Integrations', label: 'Integrations' }
   ];
+
+  integrationStepLabelMap: any = {
+    winbeat: 'WinBEAT',
+    insight: 'Insight',
+    officetech: 'Officetech'
+  };
+
+  getIntegrationLabel(key: string) {
+    return this.integrationStepLabelMap[key] || key;
+  }
 }
